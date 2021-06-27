a8269303e52328ae2ed9808e73497d7e809784fd
Imaparadox <stevemarsh117@gmail.com>
2021-06-27 13:56:04 -0400

    Connected SQL database

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '34%^Gh0stR3c0n',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
